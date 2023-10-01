d6336d6136c79478b9302567b689976970f62ba8
Benedict <ailnk@naver.com>
2023-10-02 06:04:10 +0900

    rework error page

--- blog/src/routes/Error.tsx
+++ blog/src/routes/Error.tsx
@@ -1,9 +1,16 @@
+import { Container } from 'react-bootstrap'
+
 function Error() {
   return (
-    <div>
+    <Container
+      style={{
+        height: '75vh'
+      }}
+      className="d-flex flex-column justify-content-center align-items-center"
+    >
       <h1>404 Error: Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
-    </div>
+    </Container>
   )
 }
 
