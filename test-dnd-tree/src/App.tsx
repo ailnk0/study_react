4ef58e6972e60e30324acbb248c6539e127e7559
Benedict <ailnk@naver.com>
2024-03-03 18:42:26 +0900

    test minop tree

--- test-dnd-tree/src/App.tsx
+++ test-dnd-tree/src/App.tsx
@@ -1,35 +1,45 @@
-import { Box, Typography } from "@mui/material";
+import { Box, CssBaseline, Typography } from "@mui/material";
 import BeautifulDndTreePage from "./features/dndtree/BeautifulDndTreePage";
 import SortableTreePage from "./features/dndtree/SortableTreePage";
 import AntTreePage from "./features/dndtree/AntTreePage";
 import TestPage from "./features/dndtree/BeautifulDndCardPage";
+import MinopTree from "./features/minopTree/MinopTree";
 
 function App() {
   return (
     <Box>
-      <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
-        <Typography variant="h4" gutterBottom>
-          BeautifulDndCardPage
-        </Typography>
-        <TestPage />
-      </Box>
-      <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
-        <Typography variant="h4" gutterBottom>
-          BeautifulDndTreePage
-        </Typography>
-        <BeautifulDndTreePage />
-      </Box>
-      <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
-        <Typography variant="h4" gutterBottom>
-          SortableTreePage
-        </Typography>
-        <SortableTreePage />
-      </Box>
-      <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
-        <Typography variant="h4" gutterBottom>
-          AntTreePage
-        </Typography>
-        <AntTreePage />
+      <CssBaseline />
+      <Box>
+        <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
+          <Typography variant="h4" gutterBottom>
+            MinopTree
+          </Typography>
+          <MinopTree />
+        </Box>
+        <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
+          <Typography variant="h4" gutterBottom>
+            BeautifulDndCardPage
+          </Typography>
+          <TestPage />
+        </Box>
+        <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
+          <Typography variant="h4" gutterBottom>
+            BeautifulDndTreePage
+          </Typography>
+          <BeautifulDndTreePage />
+        </Box>
+        <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
+          <Typography variant="h4" gutterBottom>
+            SortableTreePage
+          </Typography>
+          <SortableTreePage />
+        </Box>
+        <Box sx={{ p: 5, m: 5, border: 1, borderColor: "lightgrey" }}>
+          <Typography variant="h4" gutterBottom>
+            AntTreePage
+          </Typography>
+          <AntTreePage />
+        </Box>
       </Box>
     </Box>
   );
